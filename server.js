42646a1a9fd05a552e8dd7f692f207b2d5bae419
Aakash <akashkushi111@gmail.com>
2020-10-30 16:58:26 +0530

    first commit

--- server.js
+++ server.js
@@ -35,4 +35,12 @@ io.on('connect',(socket)=>{
  })
 })
 
+if(process.env.NODE_ENV==='production'){
+  app.use(express.static('client/build'))
+  app.get('*',(req,res)=>{
+   res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+  })
+}
+
+
 server.listen(PORT,()=>{console.log(`server started at ${PORT}`)})
